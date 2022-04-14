acec11b66fe571fcbd7e264a3efe1dc77f245d78
Pranesh Pyara Shrestha <80845871+praneshp1org@users.noreply.github.com>
2022-04-14 10:56:43 +0545

    bNav

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -11,18 +11,51 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int selIndex = 1;
+  void setValue(int a) {
+    setState(() {
+      selIndex = a;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(),
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: (int value) {
+            setValue(value);
+          },
+          currentIndex: selIndex,
+          selectedItemColor: Colors.orange,
+          unselectedItemColor: Colors.black54,
+          showUnselectedLabels: true,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_cart), label: 'Cart'),
+            BottomNavigationBarItem(icon: Icon(Icons.info), label: 'Info'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ]),
       appBar: AppBar(
+        actions: [
+          Icon(Icons.shopping_cart),
+        ],
+        leading: Builder(
+            builder: (context) => IconButton(
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+                icon: Icon(Icons.menu_book))),
         title: Text("Homepage"),
       ),
       body: Center(
         child: RaisedButton(
           onPressed: () {
             Navigator.popAndPushNamed(context, "/page1");
+            //selIndex++;
           },
-          child: Text("Go to Page1"),
+          child: Text("Change index"),
         ),
       ),
     );
