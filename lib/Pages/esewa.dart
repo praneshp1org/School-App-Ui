a7c0e61974ef1df68b2f033bb64c271951aac348
Pranesh Pyara Shrestha <80845871+praneshp1org@users.noreply.github.com>
2022-04-16 20:08:45 +0545

    constructor invovation

--- lib/Pages/esewa.dart
+++ lib/Pages/esewa.dart
@@ -12,7 +12,9 @@ class _EsewaState extends State<Esewa> {
   List<products> items = [
     products("PC", "450", false,
         "https://imgs.search.brave.com/-eWW3d6u1vKgtJG-bhT1TVStKGfCTGDUowy95wYkUHY/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9sb25n/d29vZGdhcmRlbnMu/b3JnL3NpdGVzL2Rl/ZmF1bHQvZmlsZXMv/aGlnaGxpZ2h0X2lt/YWdlcy83Njc1OC5q/cGc"),
-    products("Pa452", "570", false,
+    products("Pa452", "570", true,
+        "https://imgs.search.brave.com/5FByKhGK6xWtbLgUOxTPbTYSKIt1v52QNeMPeJz72Lw/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9sb25n/d29vZGdhcmRlbnMu/b3JnL3NpdGVzL2Rl/ZmF1bHQvZmlsZXMv/aGlnaGxpZ2h0X2lt/YWdlcy8xMzAzNjZf/MC5qcGc"),
+    products("Pa452", "578", true,
         "https://imgs.search.brave.com/5FByKhGK6xWtbLgUOxTPbTYSKIt1v52QNeMPeJz72Lw/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9sb25n/d29vZGdhcmRlbnMu/b3JnL3NpdGVzL2Rl/ZmF1bHQvZmlsZXMv/aGlnaGxpZ2h0X2lt/YWdlcy8xMzAzNjZf/MC5qcGc"),
   ];
 
@@ -37,6 +39,9 @@ class _EsewaState extends State<Esewa> {
                       ),
                       title: Text(items[index].productName),
                       subtitle: Text(items[index].price),
+                      trailing: items[index].sale
+                          ? Text("Available")
+                          : Text("Not available"),
                     ),
                   );
                 }))
