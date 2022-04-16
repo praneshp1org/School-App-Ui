1844a7caf1e2be99cb6ec8882e7cf7b8bb5e2a58
Pranesh Pyara Shrestha <80845871+praneshp1org@users.noreply.github.com>
2022-04-16 09:12:03 +0545

    some changes

--- lib/Pages/esewa.dart
+++ lib/Pages/esewa.dart
@@ -8,100 +8,54 @@ class Esewa extends StatefulWidget {
 }
 
 class _EsewaState extends State<Esewa> {
+  bool flag = false;
+  String str = "See more";
   @override
   Widget build(BuildContext context) {
-    //final double height = MediaQuery.of(context).size.width,
-    final double width = MediaQuery.of(context).size.width;
-    final double height = MediaQuery.of(context).size.height;
-
-    return SafeArea(
-      child: Scaffold(
-        body: Stack(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ESEWA"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
           children: [
-            Positioned(
-              top: 0,
-              left: 0,
-              child: Container(
-                height: height * 0.25,
-                width: width,
-                color: Colors.green,
-              ),
+            RaisedButton(
+              onPressed: () {
+                setState(() {
+                  if (flag == true) {
+                    flag = false;
+                    str = "See More";
+                  } else {
+                    flag = true;
+                    str = "Show less";
+                  }
+                  print(flag);
+                });
+              },
+              child: Text("$str"),
             ),
-            Positioned(
-              top: height * 0.2,
-              left: 20,
-              child: Container(
-                height: height * 0.2,
-                width: width - 40,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                ),
-              ),
-            ),
-            Positioned(
-              top: height * 0.41,
-              left: 20,
-              child: Container(
-                height: height * 0.1,
-                width: width - 40,
-                child: ListTile(
-                  title: Text("Pranesh Pyara Shrestha"),
-                  subtitle:
-                      Text("Electronics, Communication & Information Engineer"),
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-              ),
-            ),
-            Positioned(
-              top: height * 0.52,
-              left: 20,
-              child: Container(
-                height: height * 0.1,
-                width: width - 40,
-                child: ListTile(
-                  title: Text("Pranesh Pyara Shrestha"),
-                  subtitle:
-                      Text("Electronics, Communication & Information Engineer"),
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.yellow,
-                ),
-              ),
-            ),
-            Positioned(
-              top: height * 0.64,
-              left: 20,
-              child: Container(
-                height: height * 0.1,
-                width: width - 40,
-                child: ListTile(
-                  title: Text("Pranesh Pyara Shrestha"),
-                  subtitle:
-                      Text("Electronics, Communication & Information Engineer"),
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                ),
-              ),
-            ),
-            Positioned(
-              top: height * 0.78,
-              left: 20,
-              child: Container(
-                height: height * 0.1,
-                width: width - 40,
-                child: ListTile(
-                  title: Text("Pranesh Pyara Shrestha"),
-                  subtitle:
-                      Text("Electronics, Communication & Information Engineer"),
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-              ),
-            ),
+            flag == false
+                ? SizedBox()
+                : Container(
+                    child: Column(
+                      children: [
+                        Text("IOE"),
+                        Text("Institute of Engineering"),
+                        Text("IOE"),
+                        Text("Institute of Engineering"),
+                        Text("IOE"),
+                        Text("Institute of Engineering"),
+                        Text("IOE"),
+                        Text("Institute of Engineering"),
+                        Text("IOE"),
+                        Text("Institute of Engineering"),
+                        Text("IOE"),
+                        Text("Institute of Engineering"),
+                        Text("IOE"),
+                        Text("Institute of Engineering"),
+                      ],
+                    ),
+                  ),
           ],
         ),
       ),
