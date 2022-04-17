88e5a31325a3822bd17b4c9f9f6e9877165a2729
Pranesh Pyara Shrestha <80845871+praneshp1org@users.noreply.github.com>
2022-04-17 09:21:55 +0545

    new screen

---
+++ lib/Pages/splash_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("e-Shop"),
+      ),
+    );
+  }
+}
